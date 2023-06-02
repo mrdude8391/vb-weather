da048321d30c1e0fc854810649979f50d5c170f1
Bryan <h.bryan68@yahoo.ca>
2023-06-01 22:26:16 -0400

    Added form for searching location

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -2,6 +2,7 @@ import { WeatherService } from './../../services/weather-service/weather.service
 import { Weather, WeatherML } from 'src/app/interfaces/weather';
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
@@ -10,11 +11,12 @@ import { Component } from '@angular/core';
 export class WeatherComponent {
   //Properties
   localWeather: WeatherML | undefined;
+  location = "Toronto";
   //Constructor
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
-    this.getLocalWeather();
+    this.getLocalWeather(this.location);
   }
 
   //Methods
@@ -26,8 +28,14 @@ export class WeatherComponent {
     });
   }
 
-  getLocalWeather() {
-    this.weatherService.getPosts()
+  getLocation(locationForm: any){
+    console.log("Form Submitted", locationForm);
+    this.location = locationForm.location;
+    this.getLocalWeather(this.location);
+  }
+  
+  getLocalWeather(location: string) {
+    this.weatherService.getPosts(location)
     .subscribe(
       {
         next: (val: WeatherML) => {
