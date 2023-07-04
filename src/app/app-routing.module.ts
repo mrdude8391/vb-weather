1af97a7069866ec06425fb3ac8a3e08664828593
Bryan <h.bryan68@yahoo.ca>
2023-07-04 14:20:49 -0400

    Added route for home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WeatherComponent } from './components/weather/weather.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', pathMatch: 'full', redirectTo: 'home'},
+  { path:'home', component: WeatherComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
